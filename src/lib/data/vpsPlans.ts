c1afdc7a9d93c43857d993d3bb907c68ffc64b0b
Owen-sz <owen@fyralabs.com>
2026-04-30 19:25:59 -0500

    chore: update VPS specs
    Signed-off-by: Owen-sz <owen@fyralabs.com>

--- src/lib/data/vpsPlans.ts
+++ src/lib/data/vpsPlans.ts
@@ -1,12 +1,12 @@
 export const vpsPlans = [
 	{ name: 'STACK-XXS', price: '5', cpu: '2', ram: '2', storage: '40', network: '1' },
-	{ name: 'STACK-XS', price: '10', cpu: '2', ram: '4', storage: '80', network: '1' },
-	{ name: 'STACK-S', price: '15', cpu: '3', ram: '6', storage: '120', network: '1' },
-	{ name: 'STACK-M', price: '18', cpu: '4', ram: '8', storage: '160', network: '1' },
-	{ name: 'STACK-L', price: '21', cpu: '5', ram: '10', storage: '200', network: '1' },
-	{ name: 'STACK-XL', price: '25', cpu: '6', ram: '12', storage: '240', network: '1' },
-	{ name: 'STACK-XXL', price: '29', cpu: '7', ram: '14', storage: '280', network: '1' },
-	{ name: 'STACK-XXXL', price: '34', cpu: '8', ram: '16', storage: '320', network: '1' }
+	{ name: 'STACK-XS', price: '10', cpu: '2', ram: '4', storage: '60', network: '1' },
+	{ name: 'STACK-S', price: '15', cpu: '3', ram: '6', storage: '80', network: '1' },
+	{ name: 'STACK-M', price: '20', cpu: '4', ram: '8', storage: '100', network: '1' },
+	{ name: 'STACK-L', price: '25', cpu: '5', ram: '10', storage: '120', network: '1' },
+	{ name: 'STACK-XL', price: '30', cpu: '6', ram: '12', storage: '140', network: '1' },
+	{ name: 'STACK-XXL', price: '35', cpu: '7', ram: '14', storage: '160', network: '1' },
+	{ name: 'STACK-XXXL', price: '40', cpu: '8', ram: '16', storage: '180', network: '1' }
 ] as const;
 
 export type VpsPlan = (typeof vpsPlans)[number];
