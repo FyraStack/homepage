6445bdc7231778bc4a7cdd9cc4de7086cb9d99f9
Caleb Jones <caleb@calebgj.io>
2026-06-23 21:05:28 -0500

    fix markdown link highlighting hover issue
    Signed-off-by: Caleb Jones <caleb@calebgj.io>

--- src/routes/docs/colocation/+layout.svelte
+++ src/routes/docs/colocation/+layout.svelte
@@ -84,7 +84,7 @@
 			prose-h3:mt-6 prose-h3:mb-1 prose-h3:text-sm prose-h3:font-semibold prose-h3:text-fyra-gray-200
 			prose-p:text-sm prose-p:leading-relaxed prose-p:text-fyra-gray-400
 			prose-a:font-medium prose-a:text-fyra-red-400
-			prose-a:no-underline hover:prose-a:text-fyra-red-300 prose-strong:text-fyra-gray-200 prose-code:rounded
+			prose-a:no-underline prose-a:hover:text-fyra-red-300 prose-strong:text-fyra-gray-200 prose-code:rounded
 			prose-code:bg-fyra-gray-800 prose-code:px-1.5
 			prose-code:py-0.5
 			prose-code:text-sm prose-code:font-normal prose-code:text-fyra-red-300 prose-code:before:content-none prose-code:after:content-none prose-pre:border prose-pre:border-fyra-gray-700 prose-pre:bg-fyra-gray-950 prose-pre:text-fyra-gray-200
