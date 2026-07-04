8c84a72f69ef2e45a1cc9ace71200ec1fbb46390
Willow C Reed <willow@willowidk.dev>
2026-07-04 14:13:34 -0600

    try to limit changes to only docs

--- src/lib/docs-prose-class.ts
+++ src/lib/docs-prose-class.ts
@@ -11,5 +11,7 @@ export const docsProseClass = [
 	'prose-code:rounded prose-code:bg-fyra-gray-800 prose-code:px-1.5 prose-code:py-0.5 prose-code:text-sm prose-code:font-normal prose-code:text-fyra-red-200 prose-code:before:content-none prose-code:after:content-none',
 	'prose-pre:my-4 prose-pre:overflow-x-auto prose-pre:rounded-none prose-pre:border prose-pre:border-fyra-gray-700 prose-pre:bg-fyra-gray-950 prose-pre:px-4 prose-pre:py-3 prose-pre:text-fyra-gray-100',
 	'[&_pre_code]:block [&_pre_code]:bg-transparent [&_pre_code]:p-0 [&_pre_code]:text-inherit [&_pre_code]:before:content-none [&_pre_code]:after:content-none',
+	'[&_.token.comment]:text-fyra-gray-400 [&_.token.prolog]:text-fyra-gray-400 [&_.token.doctype]:text-fyra-gray-400 [&_.token.cdata]:text-fyra-gray-400',
+	'[&_.token.punctuation]:text-fyra-gray-300 [&_.token.operator]:text-fyra-gray-300 [&_.token.entity]:text-fyra-gray-300 [&_.token.url]:text-fyra-gray-300',
 	'prose-li:text-sm prose-li:text-fyra-gray-300 prose-blockquote:border-fyra-red-500 prose-blockquote:text-fyra-gray-200 prose-th:text-fyra-gray-100 prose-td:text-fyra-gray-300 prose-hr:my-8 prose-hr:border-fyra-gray-700'
 ].join(' ');
